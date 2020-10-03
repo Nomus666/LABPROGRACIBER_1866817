fdaa65b7d4f6164166bf76e9f103140ad72899f4
Leax <41660941+leaxpm@users.noreply.github.com>
2020-10-03 02:41:10 -0500

    Modificacion
    Se removio la api key la cual puede ser obtenida en la pagina https://openweathermap.org/api

--- Practica1.py
+++ Practica1.py
@@ -6,9 +6,10 @@ def call(url):
     r = requests.get(url)
     return (json.loads(r.content))
 
-output.append(call("https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=4c35b48c9218dc4d08cd6eede31f455d"))
-output.append(call("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=4c35b48c9218dc4d08cd6eede31f455d"))
-output.append(call("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=4c35b48c9218dc4d08cd6eede31f455d"))
+api = input("Api key de openweathermap\t")
+output.append(call(f"https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid={api}"))
+output.append(call(f"https://api.openweathermap.org/data/2.5/weather?id=2172797&appid={api}"))
+output.append(call(f"https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={api}"))
 
 with open("practica1.txt","w+") as txt:
     for i in output:
